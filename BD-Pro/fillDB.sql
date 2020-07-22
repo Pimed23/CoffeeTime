e1a9afc5219ef0fc62f0aed2659a343c8a3e3369
Brian <b.pimed@gmail.com>
2020-07-21 20:56:05 -0500

    Scheme

--- BD-Pro/fillDB.sql
+++ BD-Pro/fillDB.sql
@@ -1,3 +1,4 @@
+USE Coffee;
 -- CARGOS
 CALL insertarCargo('GER','Gerente');
 CALL insertarCargo('EMP','Empleado');
@@ -9,6 +10,10 @@ CALL insertarSucursal('CER','Cercado','Calle San Francisto #320','998877665');
 CALL insertarSucursal('JLB','Jose Luis Bustamante y Rivero','Av. La Cultura #1245','996655443');
 CALL insertarSucursal('CAY','Cayma','Av. Cayma #236','995544332');
 
+-- GERENTE
+INSERT INTO Empleados(id_empleado, nombre, p_apellido, s_apellido, num_contacto, fech_nac, direccion, id_cargo, id_sucursal) 
+VALUES('B00','Brian','Pinto','Medina','993393953','1996-03-23','Av. Jesus #2008','GER','JLB');
+
 -- PROVEEDOR
 CALL insertarProveedor('P01','Bruno','Coffee S.A.C.','coffeesac@gmail.com','997788662');
 CALL insertarProveedor('P02','Julio','Plasticos S.A.C.','plasticossac@gmail.com','997788664');
@@ -16,31 +21,6 @@ CALL insertarProveedor('P03','Hugo','Leche S.A.C.','lechesac@gmail.com','9977886
 CALL insertarProveedor('P04','Lucia','Harina S.A.C.','harinasac@gmail.com','997788668');
 CALL insertarProveedor('P05','Pedro','Frutas S.A.C.','frutassac@gmail.com','997788660');
 CALL insertarProveedor('P06','David','Chocolates S.A.C.','chocolatessac@gmail.com','997788661');
-
--- MESAS
-CALL insertarMesa('MI1',5,'MIR');
-CALL insertarMesa('MI2',4,'MIR'); 
-CALL insertarMesa('MI3',5,'MIR'); 
-CALL insertarMesa('MI4',6,'MIR'); 
-CALL insertarMesa('MI5',2,'MIR');
-
-CALL insertarMesa('CA1',4,'CAY');
-CALL insertarMesa('CA2',4,'CAY'); 
-CALL insertarMesa('CA3',4,'CAY'); 
-CALL insertarMesa('CA4',4,'CAY'); 
-CALL insertarMesa('CA5',4,'CAY');
-
-CALL insertarMesa('CE1',5,'CER');
-CALL insertarMesa('CE2',5,'CER'); 
-CALL insertarMesa('CE3',4,'CER'); 
-CALL insertarMesa('CE4',6,'CER'); 
-CALL insertarMesa('CE5',6,'CER');
-
-CALL insertarMesa('JL1',2,'JLB');
-CALL insertarMesa('JL2',5,'JLB'); 
-CALL insertarMesa('JL3',4,'JLB'); 
-CALL insertarMesa('JL4',5,'JLB'); 
-CALL insertarMesa('JL5',6,'JLB');
 
 -- PERSONAS
 CALL insertarPersona('M00','Brian','Pinto','Medina','993393953','1996-03-23','Av. Jesus #2008');
--- BD-Pro/proceduresCoffee.sql
+++ BD-Pro/proceduresCoffee.sql
@@ -1,19 +1,18 @@
+USE Coffee;
 -- CARGOS
-DROP PROCEDURE IF EXISTS insertarCargo;
 DELIMITER $$
+DROP PROCEDURE IF EXISTS insertarCargo;
 CREATE PROCEDURE insertarCargo(
 	IN _id_cargo VARCHAR(3),
 	IN _cargo VARCHAR(50))
 BEGIN
-	START TRANSACTION;
 	INSERT INTO Cargos(id_cargo, cargo) 
 	VALUES(_id_cargo, _cargo);
-	COMMIT;
 END$$
 
 -- PERSONA
-DROP PROCEDURE IF EXISTS insertarPersona;
 DELIMITER $$
+DROP PROCEDURE IF EXISTS insertarPersona;
 CREATE PROCEDURE insertarPersona(
 	IN _id_persona VARCHAR(3),
 	IN _nombre VARCHAR(50),
@@ -23,44 +22,38 @@ CREATE PROCEDURE insertarPersona(
 	IN _fech_nac DATE,
 	IN _direccion VARCHAR(100))
 BEGIN
-	START TRANSACTION;
 	INSERT INTO Personas(id_persona, nombre, p_apellido, s_apellido, num_contacto, fech_nac, direccion) 
 	VALUES(_id_persona, _nombre, _p_apellido, _s_apellido, _num_contacto, _fech_nac, _direccion);
-	COMMIT;
 END$$
 
 -- SUCURSALES
-DROP PROCEDURE IF EXISTS insertarSucursal;
 DELIMITER $$
+DROP PROCEDURE IF EXISTS insertarSucursal;
 CREATE PROCEDURE insertarSucursal(
 	IN _id_sucursal VARCHAR(3),
 	IN _nombre VARCHAR(50),
 	IN _ubicacion VARCHAR(100),
 	IN _telefono VARCHAR(9))
 BEGIN
-	START TRANSACTION;
 	INSERT INTO Sucursales(id_sucursal, nombre, ubicacion, telefono) 
 	VALUES(_id_sucursal, _nombre, _ubicacion, _telefono);
-	COMMIT;
 END$$
 
 -- MESAS
-DROP PROCEDURE IF EXISTS insertarMesa;
 DELIMITER $$
+DROP PROCEDURE IF EXISTS insertarMesa;
 CREATE PROCEDURE insertarMesa(
 	IN _id_mesa VARCHAR(3),
 	IN _capacidad TINYINT UNSIGNED,
 	IN _id_sucursal VARCHAR(3))
 BEGIN
-	START TRANSACTION;
 	INSERT INTO Mesas(id_mesa, capacidad, id_sucursal) 
 	VALUES(_id_mesa, _capacidad, _id_sucursal);
-	COMMIT;
 END$$
 
 -- EMPLEADOS
-DROP PROCEDURE IF EXISTS insertarEmpleado;
 DELIMITER $$
+DROP PROCEDURE IF EXISTS insertarEmpleado;
 CREATE PROCEDURE insertarEmpleado(
 	IN _id_empleado VARCHAR(3),
 	IN _nombre VARCHAR(50),
@@ -73,49 +66,41 @@ CREATE PROCEDURE insertarEmpleado(
 	IN _id_cargo VARCHAR(3),
 	IN _id_sucursal VARCHAR(3))
 BEGIN
-	START TRANSACTION;
 	INSERT INTO Empleados(id_empleado, nombre, p_apellido, s_apellido, num_contacto, fech_nac, direccion, id_jefe, id_cargo, id_sucursal) 
 	VALUES(_id_empleado, _nombre, _p_apellido, _s_apellido, _num_contacto, _fech_nac, _direccion, _id_jefe, _id_cargo, _id_sucursal);
-	COMMIT;
 END$$
 
 -- PROVEEDOR
-DROP PROCEDURE IF EXISTS insertarProveedor;
 DELIMITER $$
+DROP PROCEDURE IF EXISTS insertarProveedor;
 CREATE PROCEDURE insertarProveedor(
 	IN _id_proveedor VARCHAR(3),
 	IN _nombre_contacto VARCHAR(50),
 	IN _proveedor VARCHAR(50),
 	IN _correo VARCHAR(50),
 	IN _telefono VARCHAR(9))
-
 BEGIN
-	START TRANSACTION;
 	INSERT INTO Proveedores(id_proveedor, nombre_contacto, proveedor, correo, telefono) 
 	VALUES(_id_proveedor, _nombre_contacto, _proveedor, _correo, _telefono);
-	COMMIT;
 END$$
 
 -- INSERT INSUMOS
-DROP PROCEDURE IF EXISTS insertarInsumo;
 DELIMITER $$
+DROP PROCEDURE IF EXISTS insertarInsumo;
 CREATE PROCEDURE insertarInsumo(
 	IN _id_insumo VARCHAR(3),
 	IN _nombre VARCHAR(50),
 	IN _cantidad SMALLINT UNSIGNED,
 	IN _umbral TINYINT,
 	IN _id_proveedor VARCHAR(3))
-
 BEGIN
-	START TRANSACTION;
 	INSERT INTO Insumos(id_insumo, nombre, cantidad, umbral, id_proveedor) 
 	VALUES(_id_insumo, _nombre, _cantidad, _umbral, _id_proveedor);
-	COMMIT;
 END$$
 
 -- INSERT CLIENTE
-DROP PROCEDURE IF EXISTS insertarCliente;
 DELIMITER $$
+DROP PROCEDURE IF EXISTS insertarCliente;
 CREATE PROCEDURE insertarCliente(
 	IN _id_cliente VARCHAR(3),
 	IN _nombre VARCHAR(50),
@@ -124,15 +109,13 @@ CREATE PROCEDURE insertarCliente(
 	IN _num_contacto VARCHAR(9))
 
 BEGIN
-	START TRANSACTION;
 	INSERT INTO Clientes(id_cliente, nombre, p_apellido, s_apellido, num_contacto) 
 	VALUES(_id_cliente, _nombre, _p_apellido, _s_apellido, _num_contacto);
-	COMMIT;
 END$$
 
 -- BUSCAR EMPLEADO
-DROP PROCEDURE IF EXISTS buscarEmpleado;
 DELIMITER $$
+DROP PROCEDURE IF EXISTS buscarEmpleado;
 CREATE PROCEDURE buscarEmpleado(
 	IN _id_empleado VARCHAR(3))
 BEGIN
@@ -140,8 +123,8 @@ BEGIN
 END$$
 
 -- BUSCAR INSUMO
-DROP PROCEDURE IF EXISTS buscarInsumo;
 DELIMITER $$
+DROP PROCEDURE IF EXISTS buscarInsumo;
 CREATE PROCEDURE buscarInsumo(
 	IN _id_insumo VARCHAR(3))
 BEGIN
@@ -149,8 +132,8 @@ BEGIN
 END$$
 
 -- BUSCAR CLIENTE
-DROP PROCEDURE IF EXISTS buscarCliente;
 DELIMITER $$
+DROP PROCEDURE IF EXISTS buscarCliente;
 CREATE PROCEDURE buscarCliente(
 	IN _id_cliente VARCHAR(3))
 BEGIN
@@ -158,8 +141,8 @@ BEGIN
 END$$
 
 -- SELECT EMPLEADO
-DROP PROCEDURE IF EXISTS selectEmpleado;
 DELIMITER $$
+DROP PROCEDURE IF EXISTS selectEmpleado;
 CREATE PROCEDURE selectEmpleado()
 BEGIN
 	SELECT a.id_empleado, a.p_apellido, a.s_apellido, a.nombre, a.fech_nac, a.num_contacto, a.direccion, b.nombre as 'jefe' FROM Empleados a
@@ -168,16 +151,16 @@ ORDER BY a.id_empleado;
 END$$
 
 -- SELECT CLIENTE
-DROP PROCEDURE IF EXISTS selectCliente;
 DELIMITER $$
+DROP PROCEDURE IF EXISTS selectCliente;
 CREATE PROCEDURE selectCliente()
 BEGIN
 	SELECT * FROM Clientes;
 END$$
 
 -- SELECT INSUMOS
-DROP PROCEDURE IF EXISTS selectInsumo;
 DELIMITER $$
+DROP PROCEDURE IF EXISTS selectInsumo;
 CREATE PROCEDURE selectInsumo()
 BEGIN
 	SELECT id_insumo, nombre, cantidad, umbral, proveedor FROM Insumos a
@@ -185,8 +168,8 @@ INNER JOIN Proveedores b ON a.id_proveedor = b.id_proveedor;
 END$$
 
 -- ACTUALIZAR EMPLEADO
-DROP PROCEDURE IF EXISTS actualizarEmpleado;
 DELIMITER $$
+DROP PROCEDURE IF EXISTS actualizarEmpleado;
 CREATE PROCEDURE actualizarEmpleado(
 	IN _id_empleado VARCHAR(3),
 	IN _nombre VARCHAR(50),
@@ -198,14 +181,12 @@ CREATE PROCEDURE actualizarEmpleado(
 	IN _id_jefe VARCHAR(3),
 	IN _id_cargo VARCHAR(3))
 BEGIN
-	START TRANSACTION;
 	UPDATE Empleados SET nombre = _nombre, p_apellido = _p_apellido, s_apellido = _s_apellido, num_contacto = _num_contacto, fech_nac = _fech_nac, direccion = _direccion, id_jefe = _id_jefe, id_cargo = _id_cargo WHERE id_empleado = _id_empleado;
-	COMMIT;
 END$$
 
 -- ACTUALIZAR CLIENTE
-DROP PROCEDURE IF EXISTS actualizarCliente;
 DELIMITER $$
+DROP PROCEDURE IF EXISTS actualizarCliente;
 CREATE PROCEDURE actualizarCliente(
 	IN _id_cliente VARCHAR(3),
 	IN _nombre VARCHAR(50),
@@ -214,14 +195,12 @@ CREATE PROCEDURE actualizarCliente(
 	IN _num_contacto VARCHAR(9))
 
 BEGIN
-	START TRANSACTION;
 	UPDATE Clientes SET nombre = _nombre, p_apellido = _p_apellido, s_apellido = _s_apellido, num_contacto = _num_contacto WHERE id_cliente = _id_cliente;
-	COMMIT;
 END$$
 
 -- ACTUALIZAR INSUMO
-DROP PROCEDURE IF EXISTS actualizarInsumo;
 DELIMITER $$
+DROP PROCEDURE IF EXISTS actualizarInsumo;
 CREATE PROCEDURE actualizarInsumo(
 	IN _id_insumo VARCHAR(3),
 	IN _nombre VARCHAR(50),
@@ -230,72 +209,60 @@ CREATE PROCEDURE actualizarInsumo(
 	IN _cod_proveedor VARCHAR(9))
 
 BEGIN
-	START TRANSACTION;
 	UPDATE Insumos SET nombre = _nombre, cantidad = _cantidad, umbral = _umbral, id_proveedor = _cod_proveedor WHERE id_insumo = _id_insumo;
-	COMMIT;
 END$$
 
 -- ELIMINAR EMPLEADO
-DROP PROCEDURE IF EXISTS eliminarEmpleado;
 DELIMITER $$
+DROP PROCEDURE IF EXISTS eliminarEmpleado;
 CREATE PROCEDURE eliminarEmpleado(
 	IN _id_empleado VARCHAR(3))
 
 BEGIN
-	START TRANSACTION;
 	DELETE FROM Empleados WHERE id_empleado = _id_empleado;
-	COMMIT;
 END$$
 
 -- ELIMINAR CLIENTE
-DROP PROCEDURE IF EXISTS eliminarCliente;
 DELIMITER $$
+DROP PROCEDURE IF EXISTS eliminarCliente;
 CREATE PROCEDURE eliminarCliente(
 	IN _id_cliente VARCHAR(3))
 
 BEGIN
-	START TRANSACTION;
 	DELETE FROM Clientes WHERE id_cliente = _id_cliente;
-	COMMIT;
 END$$
 
 -- ELIMINAR INSUMO
-DROP PROCEDURE IF EXISTS eliminarInsumo;
 DELIMITER $$
+DROP PROCEDURE IF EXISTS eliminarInsumo;
 CREATE PROCEDURE eliminarInsumo(
 	IN _id_insumo VARCHAR(3))
 
 BEGIN
-	START TRANSACTION;
 	DELETE FROM Insumos WHERE id_insumo = _id_insumo;
-	COMMIT;
 END$$
 
 -- FILTRAR EMPLEADO
-DROP PROCEDURE IF EXISTS filtrarEmpleado;
 DELIMITER $$
+DROP PROCEDURE IF EXISTS filtrarEmpleado;
 CREATE PROCEDURE filtrarEmpleado(
 	IN _word VARCHAR(3))
 
 BEGIN
-	START TRANSACTION;
 	SELECT a.id_empleado, a.p_apellido, a.s_apellido, a.nombre, a.fech_nac, a.num_contacto, a.direccion, b.nombre as 'jefe' FROM Empleados a
 	INNER JOIN Empleados b ON a.id_jefe = b.id_empleado
 	WHERE a.nombre LIKE _word
 	ORDER BY a.id_empleado;
-	COMMIT;
 END$$
 
 -- FILTRAR CLIENTE
-DROP PROCEDURE IF EXISTS filtrarCliente;
 DELIMITER $$
+DROP PROCEDURE IF EXISTS filtrarCliente;
 CREATE PROCEDURE filtrarCliente(
 	IN _word VARCHAR(3))
 
 BEGIN
-	START TRANSACTION;
 	SELECT * FROM Clientes WHERE nombre LIKE _word;
-	COMMIT;
 END$$
 
 
